c58b3a115dcd00af1bfe5facfb60862373a073f8
Andreas Lang <Andreas_Lang@trimble.com>
2016-12-01 16:09:29 +0100

    Improved (separated in procedures, better JSON and error handling)

--- tc_test.pl
+++ tc_test.pl
@@ -12,109 +12,86 @@ use HTTP::Request::Common;
 use Data::Dumper;
 use JSON;
 
-### THIS CODE CAN ONLY BE USED ONCE!
-#my $code="21237dee408950628f9d19a70c728fc";
+# using JSON package
+# see http://xmodulo.com/how-to-parse-json-string-in-perl.html
+# and https://www.tutorialspoint.com/json/json_perl_example.htm
 
-my $tcRootUrl = "https://staging.qa1.gteam.com/tc/api/2.0";
-my $id_token = "";
-#my $id_token="eyJhbGcixxxxx";
+our $gTCRootUrl = "https://staging.qa1.gteam.com/tc/api/2.0";
+our $gIdToken = "";
 
 if (defined $ENV{'ID_TOKEN'}) {
-	$id_token = $ENV{'ID_TOKEN'};
+	$gIdToken = $ENV{'ID_TOKEN'};
 }
 else {
 	print stderr "ERROR: environment variable ID_TOKEN not found\n";
 	exit 1;
 }
 
+# get token
+sub get_token {
+	my $token = "";
+	my $getTokenUrl = $gTCRootUrl . "/auth/token";
+	print "getTokenUrl=$getTokenUrl\n";
 
-#my $server_endpoint = "https://identity-stg.trimble.com/i/oauth2/token";
-my $server_endpoint = $tcRootUrl . "/auth/token";
-print "server_endpoint=$server_endpoint\n";
+	my %rec_hash = ('jwt' => $gIdToken);
+	my $json = encode_json \%rec_hash;		
+	#print "json=$json\n";
+	
+	my $req = HTTP::Request->new(POST => $getTokenUrl);
+	$req->header('Content-Type' => 'application/json');
+	$req->content($json);
 
-#my $rediruri="http://localhost:8888/auth_trimbleid/oauth_after.html";
-#my $redirurienc="http%3A%2F%2Flocalhost%3A8888%2Fauth_trimbleid%2Foauth_after.html";
-#my $authstr="Basic SEE3NG02UFBZN1NzX19zejBVTVVER2ltTVlZYTpYcFZxQmYyY1kyZ0UwVzdxeUhZOXNPdFBOZmdh";
+	my $ua = LWP::UserAgent->new;
+	my $resp = $ua->request($req);
+	#print Dumper(\%$resp);
+	my $json_string = "";
+	if ($resp->is_success) {
+		my $message = $resp->decoded_content;
+		print "Received reply: $message\n";
+		$json_string = $message;
+	}
+	else {
+		print "HTTP POST error code: ", $resp->code, "\n";
+		print "HTTP POST error message: ", $resp->message, "\n";
+		return "";
+	}
 
-#my $contentstr = "grant_type=authorization_code&tenantDomain=trimble.com&code=" . $code . "&redirect_uri=" . $redirurienc;
-#my $contentstr = "{ \"jwt\": \"xxxxxxxx.xxxxxxx.xxxxx\" }";
-my $contentstr = "{ \"jwt\": \"$id_token\" }";
-print "contentstr=$contentstr\n";
+	my $json_data = decode_json($json_string);
+	#print Dumper $json_data;
 
-my $posturl = $server_endpoint;
-print "POST $posturl\n";
-
-my $req = HTTP::Request->new(POST => $posturl);
-#$req->header("Authorization" => $authstr);
-#$req->header('Content-Type' => 'application/x-www-form-urlencoded');
-$req->header('Content-Type' => 'application/json');
-#$req->header('Accept' => 'application/json');
-#$req->header('Cache-Control' => 'no-cache');
-#$req->header('Host' => 'identity-stg.trimble.com');
-$req->content($contentstr);
-
-my $ua = LWP::UserAgent->new;
-my $resp = $ua->request($req);
-#print Dumper(\%$resp);
-my $json_string = "";
-if ($resp->is_success) {
-	my $message = $resp->decoded_content;
-	print "Received reply: $message\n";
-	$json_string = $message;
-}
-else {
-	print "HTTP POST error code: ", $resp->code, "\n";
-	print "HTTP POST error message: ", $resp->message, "\n";
+	$token = $json_data->{'token'};
+	print " ==> token='$token'\n";
+	
+	return $token;
 }
 
-#my $json_string = join '', <han1>;
-my $json_data = decode_json($json_string);
-print Dumper $json_data;
+# list the projects
+sub list_projects {
+	my $headerAuthStr = shift;
 
-#print "token: " . $json_data->{'token'} . "\n";
+	my $getProjectListUrl = $gTCRootUrl . "/projects";
+	print "getProjectListUrl=$getProjectListUrl\n";
 
-my $token = $json_data->{'token'};
-print "token='$token'\n";
+	my $req = HTTP::Request->new(GET => $getProjectListUrl);
+	$req->header("Authorization" => $headerAuthStr);
+	$req->header('Content-Type' => 'application/json');
 
-#foreach my $section (@$json_data) {
-#	print "section
-#    print "token: " . $section->{'token'} . "\n";
-#}
-my $server_endpoint = $tcRootUrl . "/projects";
-print "server_endpoint=$server_endpoint\n";
+	my $ua = LWP::UserAgent->new;
+	my $resp = $ua->request($req);
+	print Dumper(\%$resp);
+}
 
-#my $rediruri="http://localhost:8888/auth_trimbleid/oauth_after.html";
-#my $redirurienc="http%3A%2F%2Flocalhost%3A8888%2Fauth_trimbleid%2Foauth_after.html";
-my $authstr="Bearer $token";
+# get token for the action
+my $token = get_token();
 
-#my $contentstr = "grant_type=authorization_code&tenantDomain=trimble.com&code=" . $code . "&redirect_uri=" . $redirurienc;
-#my $contentstr = "{ \"jwt\": \"xxxxxxxx.xxxxxxx.xxxxx\" }";
-#my $contentstr = "{ \"jwt\": \"$id_token\" }";
-#print "contentstr=$contentstr\n";
+if ($token eq "") {
+	print stderr "ERROR: could not receive a token - ID_TOKEN expired?\n";
+	exit 1;
+}
 
-my $posturl = $server_endpoint;
-print "POST $posturl\n";
-
-my $req = HTTP::Request->new(GET => $posturl);
-$req->header("Authorization" => $authstr);
-#$req->header('Content-Type' => 'application/x-www-form-urlencoded');
-$req->header('Content-Type' => 'application/json');
-#$req->header('Accept' => 'application/json');
-#$req->header('Cache-Control' => 'no-cache');
-#$req->header('Host' => 'identity-stg.trimble.com');
-$req->content($contentstr);
-
-my $ua = LWP::UserAgent->new;
-my $resp = $ua->request($req);
-print Dumper(\%$resp);
-#if ($resp->is_success) {
-#	my $message = $resp->decoded_content;
-#	print "Received reply: $message\n";
-#}
-#else {
-#	print "HTTP POST error code: ", $resp->code, "\n";
-#	print "HTTP POST error message: ", $resp->message, "\n";
-#}
+# list projects using authentication with the received token
+my $headerAuthStr="Bearer $token";
+list_projects($headerAuthStr);
 
 exit 0;
 
